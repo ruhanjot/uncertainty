f61495d4bafba5002b27d8d291e7d63db02b3cb6
jaswinder bhandohal <ruhanjot@gmail.com>
2019-01-08 22:05:04 -0700

    UI now has an example of how to use the class

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 from Values import Values
-my = Values(10, 1)
-the = Values(30, 6)
-test = Values(3, 0.5)
-print(2 - test / the)
+var1 = Values(10, 1)
+var2 = Values(30, 6)
+var3 = Values(3, 0.5)
+ans = 2 - var2 / (var3 ** 3 + var1)
+print(ans)
